c9c8e0c1d19b81ac220813a776532275e53ba0a5
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-10 16:58:48 +0100

    feat: add BookPage view

---
+++ src/components/pages/BookPage.vue
@@ -0,0 +1,34 @@
+<script>
+import BookCard from "../books/BookCard.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      books: {},
+      baseUrl: "http://127.0.0.1:8000/api/",
+    };
+  },
+
+  components: {
+    BookCard,
+  },
+
+  created() {
+    console.log(this.$route.params.id);
+    axios
+      .get(this.baseUrl + "books/" + this.$route.params.id)
+      .then((response) => {
+        this.book = response.data;
+      });
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <BookCard :book="book" :detailView="true" v-if="book"> </BookCard>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
